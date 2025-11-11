a0251e2c39063274d01c9cc5f2f9ce36bdb6dd2e
Szymon Wieczorek <doszymona@gmail.com>
2025-11-11 13:50:51 +0100

    Remove install full dialog

--- lib/widgets/dialogs/demo_version_dialog.dart
+++ lib/widgets/dialogs/demo_version_dialog.dart
@@ -5,31 +5,32 @@ class DemoVersionDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned.fill(
-      child: IgnorePointer(
-        ignoring: true, // sprawia, że dotyk przechodzi do elementów pod spodem
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Colors.black.withAlpha(50),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Text(
-                "To jest demo wersja aplikacji.\nPełna wersja testownika jest dostępna w sklepie Google Play i App Store.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white.withAlpha(100),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return const SizedBox();
+    // return Positioned.fill(
+    //   child: IgnorePointer(
+    //     ignoring: true, // sprawia, że dotyk przechodzi do elementów pod spodem
+    //     child: Center(
+    //       child: Padding(
+    //         padding: const EdgeInsets.all(8.0),
+    //         child: Container(
+    //           padding: const EdgeInsets.all(16),
+    //           decoration: BoxDecoration(
+    //             color: Colors.black.withAlpha(50),
+    //             borderRadius: BorderRadius.circular(12),
+    //           ),
+    //           child: Text(
+    //             "To jest demo wersja aplikacji.\nPełna wersja testownika jest dostępna w sklepie Google Play i App Store.",
+    //             textAlign: TextAlign.center,
+    //             style: TextStyle(
+    //               color: Colors.white.withAlpha(100),
+    //               fontSize: 24,
+    //               fontWeight: FontWeight.bold,
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
